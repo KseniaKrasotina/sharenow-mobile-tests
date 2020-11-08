7e6c811c643e9acf15239571216f28a83d552562
Kseniya Krasotina <delovaya86@gmail.com>
2020-11-08 23:40:46 +0100

    some locators for RegistrationFormPage added

--- src/test/java/page_object/Android/RegistrationFormPage.java
+++ src/test/java/page_object/Android/RegistrationFormPage.java
@@ -1,44 +1,40 @@
 package page_object.Android;
 
-import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class RegistrationFormPage {
-    private WebDriver driver;
+    private AndroidDriver driver;
 
     public RegistrationFormPage() {
     }
 
-    public RegistrationFormPage(WebDriver driver) {
+    public RegistrationFormPage(AndroidDriver driver) {
         this.driver = driver;
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
 
-    @AndroidFindBy(id = "camelot-select-52072")
-    private WebElement SelectCountry;
+    @FindBy(xpath = "//select[@name=\"addressCountryIsoCode\"]")
+    private AndroidElement SelectCountry;
 
-    @AndroidFindBy(xpath = "//input[@name=\"email\"]")
+    @FindBy(xpath = "//input[@name=\"email\"]")
     private WebElement Email;
-
-    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[8]/android.view.View/android.widget.EditText")
-    private WebElement Email2;
 
     public void clickSelectCountry() {
         SelectCountry.click();
+    }
+
+    public void selectCountry(Integer country) {
+        SelectCountry.setId(String.valueOf(country));
     }
 
     public void typeEmail(String email) {
         Email.clear();
         Email.sendKeys(email);
     }
-
-    public void typeEmail2(String email) {
-        Email2.clear();
-        Email2.sendKeys(email);
-    }
-
 
 }
